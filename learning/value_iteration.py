1d7ffb21fa11ca4809d8b23465723b53c75b5f1b
BounharAbdelaziz <bounhar.abdelaziz@gmail.com>
2024-01-20 17:27:16 +0100

    Value Iteration imp

---
+++ learning/value_iteration.py
@@ -0,0 +1,111 @@
+import numpy as np
+from environment.env import Environnement
+
+class ValueIterationAgent():
+    
+    def __init__(self, environment: Environnement, gamma: float = 0.99, convergence_threshold: float = 1e-5):
+        """
+        Class for Value Iteration Algorithm
+
+        Parameters
+        ----------
+            - environment : Model for the State : (User 1: (buffer1, channel 1), User 2 : (buffer2, channel2))
+            - gamma : Discount Factor (default = 0.99)
+            - convergence_threshold : Convergence Criterion (default = 1e-5)
+        """
+        
+        self.env = environment
+        self.gamma = gamma
+        self.convergence_threshold = convergence_threshold
+        
+    def train(self,):
+        """
+        Train the Value Iteration Agent.
+        """
+        print("Value Iteration Training : Process Initiated ... ")
+        V, policy = self.value_iteration()
+        print("Value Iteration Training : Process Completed !")
+        print(f'Value function: {V}')
+        print(f'Policy: {policy}')
+        
+        
+        # print("Value Iteration Training : Saving the Policy and Value Function ... ")
+        # np.savez('policy.npz', policy)
+        # np.savez('vFunc.npz', V)
+        # print("Value Iteration Training : Policy and Value Function Saved !")
+        
+        return V, policy
+    
+    def value_iteration(self,):
+        """
+        Value Iteration Algorithm.
+            
+        Returns:
+            A tuple (V, policy) of the optimal value function and the optimal policy.
+        """
+        
+        # state and action space sizes
+        n_states = self.env.state_space.n_states
+        n_actions = self.env.action_space.n_actions
+        
+        def one_step_lookahead(state_index, V):
+            """
+            Function to calculate the value for all actions [A] in given state s.
+            
+            Args:
+                state_index: The state to consider (int)
+                V: The value to use as an estimator, Vector of length env.nS
+            
+            Returns:
+                A vector of length env.nA containing the expected value of each action.
+            """
+            A = np.zeros(n_actions)
+            # transitions = self.env.p(state_index, action_index)
+            for action_index in range(n_actions):
+                # transitions = self.env.p(state_index, action_index)
+                # next_state_prob, next_state_index, reward = transitions[action_index]
+                
+                # it's a bit strange as env.p returns a list of tuples (next_state_prob, next_state_index, reward)
+                next_state_prob, next_state_index, reward = self.env.p(state_index, action_index)
+                
+                A[action_index] += next_state_prob * (reward + self.gamma * V[next_state_index])
+            return A
+
+        #Initialization value function
+        V = np.zeros(n_states)
+        policy = np.zeros([n_states, n_actions])
+
+        #While not optimal value function/optimal policy
+        while True:
+            # Stopping condition
+            delta = 0
+            
+            # Update each state
+            for state_index in range(n_states):
+                
+                # Do a one-step lookahead to find the best action 
+                A = one_step_lookahead(state_index, V)
+                print(f'A: {A}')
+                # choose the action that maximizes the state-value function
+                best_action_value = np.max(A)
+                
+                # Calculate delta across all states 
+                delta = max(delta, np.abs(best_action_value - V[state_index]))
+                print(f'delta: {delta}')
+                
+                print('----------------------------------------------------------')
+                # Update the value function: Bellman optimality eqn 
+                V[state_index] = best_action_value
+            print(V)
+            # Check for convergence 
+            if delta < self.convergence_threshold:
+                break
+        
+        # Create a deterministic policy using the optimal value function
+        for state_index in range(n_states):
+            # One step lookahead to find the best action for this state
+            A = one_step_lookahead(state_index, V)
+            best_action = np.argmax(A)
+            # Always take the best action
+            policy[state_index, best_action] = 1.0
+        return V, policy
