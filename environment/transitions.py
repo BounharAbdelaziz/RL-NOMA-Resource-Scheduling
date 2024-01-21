92e13dd6ec6d03cadb3baff39a3afa2fd855e2bd
IbraDje <ibrahim.djemai@telecom-sudparis.eu>
2024-01-21 04:06:37 +0100

    fixed the issue with the transition matrix not outputing correct probabilities

--- environment/transitions.py
+++ environment/transitions.py
@@ -16,16 +16,16 @@ class Transition():
         
         self.transition_matrix = self.__compute_transitions()
         
-        print(f'Transition Matrix: {self.transition_matrix}')
+        # print(f'Transition Matrix: {self.transition_matrix}')
 
     def __compute_transitions(self):
         
         transition_matrix = np.ones(self.transition_matrix_dims)
         
-        
         for s in range(self.transition_matrix_dims[0]):
             # current_state = [(data_packets, snr_level, battery_level), (...), ...]
             current_state = self.states.get_state_from_index(state_index=s)
+            actions_count = 0
             for a in range(self.transition_matrix_dims[1]):
                 action_dictionary = self.actions.get_action_dictionary(a)
                 noma_users_snr = []
@@ -35,17 +35,21 @@ class Transition():
                     # the action number of packets to execute for the user are not available in the user buffer
                     if action_dictionary[f'user_{l+1}'] > user_current_state[0]:
                         transition_matrix[s, a, :] = 0
+                        break
                     
                     if action_dictionary[f'action_{l+1}'] == 'communicate':
                         # log the communicating users to verify multiple communications
-                        noma_users_snr.append(user_current_state[1])
+                        if user_current_state[1] > 1:
+                            noma_users_snr.append(user_current_state[1])
                         # the user action dictates that the user communicates, but its SNR does not allow it 
                         if user_current_state[1] == 0:
                             transition_matrix[s, a, :] = 0
+                            break
                     
                     # the user battery is not sufficient to execute the action number of packets
                     if action_dictionary[f'user_{l+1}'] > user_current_state[2]:
                         transition_matrix[s, a, :] = 0
+                        break
                 
                 # the joint action dictates multiple communications
                 if len(noma_users_snr) > 1:
@@ -53,12 +57,13 @@ class Transition():
                         # one of the user SNRs does not permit it
                         if user_snr < len(noma_users_snr):
                             transition_matrix[s, a, :] = 0
+                            break
                 
                 if transition_matrix[s, a, :].all() != 0:
+                    actions_count += 1
                     for p in range(self.transition_matrix_dims[2]):
                         next_state = self.states.get_state_from_index(state_index=p)
                         
-
                         # Verify if the action can be performed, exclude transition if :
                         for l, (user_current_state, user_next_state) in enumerate(zip(current_state, next_state)):
                         # for l, (user_current_state, user_next_state) in enumerate(current_state, next_state):
@@ -76,13 +81,21 @@ class Transition():
                                 if user_next_state[2] < (user_current_state[2] - energy_to_execute):
                                     transition_matrix[s, a, p] = 0
                         
-                        if transition_matrix[s, a, p] != 0:
-                            # Compute the transition probability from state `s` to state `p` after taking action `a`
-                            for _ in range(self.states.n_users):
-                                # Buffer transition probability
-                                transition_matrix[s, a, p] *= (1 / (self.maximum_number_of_packets + 1))
-                                # SNR transition probability
-                                transition_matrix[s, a, p] *= (1 / self.snr_levels_cardinality)
-                                # Battery transition probability
-                                transition_matrix[s, a, p] *= (1 / (self.maximum_battery_level + 1))
+                        # if transition_matrix[s, a, p] != 0:
+                        #     # Compute the transition probability from state `s` to state `p` after taking action `a`
+                        #     for _ in range(self.states.n_users):
+                        #         # Buffer transition probability
+                        #         transition_matrix[s, a, p] *= (1 / (self.maximum_number_of_packets + 1))
+                        #         # SNR transition probability
+                        #         transition_matrix[s, a, p] *= (1 / self.snr_levels_cardinality)
+                        #         # Battery transition probability
+                        #         transition_matrix[s, a, p] *= (1 / (self.maximum_battery_level + 1))
+
+                    # Alternative to the transition probability : since the all the changes in the state follow the uniform or bernoulli distributions,
+                    # compute the transition simply as the inverse of the number of available (s, a) couples (avoiding all conditions above)
+                    transition_matrix[s, a, :] = transition_matrix[s, a, :] / sum(transition_matrix[s, a, :])
+            # for the transitions from s to s', divide by the number of available actions, to get to a sum of 1 (could be optional depending on the setup)
+            if actions_count > 1:
+                print(actions_count)
+                transition_matrix[s, :, :] = transition_matrix[s, :, :] / actions_count
         return transition_matrix
