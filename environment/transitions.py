fca9ceb64b6db97cd6dd1d8bb89a4496f756f6b4
IbraDje <ibrahim.djemai@telecom-sudparis.eu>
2024-01-21 13:54:14 +0100

    additional adjustments to transition probabilities

--- environment/transitions.py
+++ environment/transitions.py
@@ -27,7 +27,7 @@ class Transition():
         for s in range(self.transition_matrix_dims[0]):
             # current_state = [(data_packets, snr_level, battery_level), (...), ...]
             current_state = self.states.get_state_from_index(state_index=s)
-            actions_count = 0
+            # actions_count = 0
             for a in range(self.transition_matrix_dims[1]):
                 action_dictionary = self.actions.get_action_dictionary(a)
                 noma_users_snr = []
@@ -62,7 +62,7 @@ class Transition():
                             break
                 
                 if transition_matrix[s, a, :].all() != 0:
-                    actions_count += 1
+                    # actions_count += 1
                     for p in range(self.transition_matrix_dims[2]):
                         next_state = self.states.get_state_from_index(state_index=p)
                         
@@ -74,36 +74,50 @@ class Transition():
                             # The next state buffer has more than `maximum_number_of_packets` energy units than the current user buffer
                             if (user_next_state[0] - user_current_state[0]) > self.maximum_number_of_packets:
                                 transition_matrix[s, a, p] = 0
-
-                            # The next state battery has more than `maximum_energy_unit_arrival` energy units than the current user battery
-                            if (user_next_state[2] - user_current_state[2]) > self.maximum_energy_unit_arrival:
-                                transition_matrix[s, a, p] = 0
+                                break
                             
                             if action_dictionary[f'action_{l+1}'] == 'idle':
+                                # The next state battery has more than `maximum_energy_unit_arrival` energy units than the current user battery
+                                if user_next_state[2] - user_current_state[2] > self.maximum_energy_unit_arrival:
+                                    transition_matrix[s, a, p] = 0
+                                    break
                                 # The user battery level in the next state is less then the user battery level in the current state
                                 if user_next_state[2] < user_current_state[2]:
                                     transition_matrix[s, a, p] = 0
+                                    break
                             
                             elif action_dictionary[f'action_{l+1}'] == 'communicate':
                                 energy_to_execute = action_dictionary[f'user_{l+1}'] # Each packet executed consumed 1 energy unit
                                 # The user battery level in the next state is less then the user battery level in the current state after taking the action
                                 if user_next_state[2] < (user_current_state[2] - energy_to_execute):
                                     transition_matrix[s, a, p] = 0
+                                    break
+                                # The next state battery has more than `maximum_energy_unit_arrival` energy units than the current user battery
+                                if user_next_state[2] > (user_current_state[2] - energy_to_execute + self.maximum_energy_unit_arrival):
+                                    transition_matrix[s, a, p] = 0
+                                    break
                         
-                        # if transition_matrix[s, a, p] != 0:
-                        #     # Compute the transition probability from state `s` to state `p` after taking action `a`
-                        #     for _ in range(self.states.n_users):
-                        #         # Buffer transition probability
-                        #         transition_matrix[s, a, p] *= (1 / (self.maximum_number_of_packets + 1))
-                        #         # SNR transition probability
-                        #         transition_matrix[s, a, p] *= (1 / self.snr_levels_cardinality)
-                        #         # Battery transition probability
-                        #         transition_matrix[s, a, p] *= (1 / (self.maximum_battery_level + 1))
-
+                        if transition_matrix[s, a, p] != 0:
+                            # Compute the transition probability from state `s` to state `p` after taking action `a`
+                            for l, user_current_state in enumerate(current_state):
+                                
+                                # Buffer transition probability
+                                transition_matrix[s, a, p] *= (1 / (self.maximum_number_of_packets + 1))
+                                
+                                # SNR transition probability
+                                transition_matrix[s, a, p] *= (1 / self.snr_levels_cardinality)
+                                
+                                # Battery transition probability
+                                energy_to_execute = action_dictionary[f'user_{l+1}']
+                                if (user_current_state[2] - energy_to_execute) < self.maximum_battery_level:
+                                    transition_matrix[s, a, p] *= (1 / (self.maximum_energy_unit_arrival + 1))
+                            transition_matrix[s, a, p] = np.round(transition_matrix[s, a, p], 6)
+                    
                     # Alternative to the transition probability : since the all the changes in the state follow the uniform or bernoulli distributions,
                     # compute the transition simply as the inverse of the number of available (s, a) couples (avoiding all conditions above)
-                    transition_matrix[s, a, :] = transition_matrix[s, a, :] / sum(transition_matrix[s, a, :])
+                    # transition_matrix[s, a, :] = transition_matrix[s, a, :] / sum(transition_matrix[s, a, :])
             # for the transitions from s to s', divide by the number of available actions, to get to a sum of 1 (could be optional depending on the setup)
-            if actions_count > 1:
-                transition_matrix[s, :, :] = transition_matrix[s, :, :] / actions_count
+            # if actions_count > 1:
+            #     transition_matrix[s, :, :] = transition_matrix[s, :, :] / actions_count
+        
         return transition_matrix
